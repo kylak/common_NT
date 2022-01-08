1dca2828fcac7e6885e5ce74643ec111fb8052a6
kylak <gustavberloty2@gmail.com>
2022-01-08 23:49:43 +0100

    Organisation en dossiers.

---
+++ c#/Infos.cs
@@ -0,0 +1,50 @@
+public class Infos {
+	
+public static string[] book = { "",
+"MATTHIEU", 
+"MARC",
+"LUC",
+"JEAN",
+"ACTES",
+"ROMAINS",
+"1 CORINTHIENS",
+"2 CORINTHIENS",
+"GALATES",
+"EPHESIENS",
+"PHILIPPIENS",
+"COLOSSIENS",
+"1 THESSALONICIENS",
+"2 THESSALONICIENS",
+"1 TIMOTHEE",
+"2 TIMOTHEE",
+"TITE",
+"PHILEMON",
+"HEBREUX",
+"JACQUES",
+"1 PIERRE",
+"2 PIERRE",
+"1 JEAN",
+"2 JEAN",
+"3 JEAN",
+"JUDAS",
+"APOCALYPSE"
+};
+
+
+public static string[] textes_critiques = {
+
+"NESTLE-ET-ALAND",
+"SBL",
+"SCRIVENER",
+"SEBASTIEN-D-G",
+"STEPHANUS",
+"TISCHENDORF",
+"WESTCOTT-ET-HORT"
+
+};
+
+	public void Main() {
+		
+	}
+	
+}
---
+++ c#/Parametres.cs
@@ -0,0 +1,12 @@
+
+public class Parametres {
+
+public static int DATA_TYPE = 0;
+
+public static string[]  textes = Infos.textes_critiques;
+
+	public void Main() {
+		
+	}
+
+}
